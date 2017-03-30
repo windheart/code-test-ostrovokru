9a23de0d5498f35fe0c82866c67cafa045360d5f
windheart <windheart.me@yandex.ru>
2017-03-30 19:09:09 +0300

    counters

--- click-counter/index.js
+++ click-counter/index.js
@@ -1,3 +1,12 @@
+function Panel() {}
+
+Panel.prototype.init = function(el) {
+  var clicks = 0;
+  el.addEventListener('click', function() {
+    document.querySelector('.' + el.className + '-counter').textContent = ++clicks;
+  });
+};
+
 function main () {
   var panelsElements = document.querySelector('.wrapper');
   [].slice.apply(panelsElements.children).forEach(function (panelElement) {
