b62a20a3701b0a9ed43f41f2bfd9dff1bfc9bf16
windheart <windheart.me@yandex.ru>
2017-03-30 18:06:48 +0300

    deep copy fix

--- object-deep-copy/index.js
+++ object-deep-copy/index.js
@@ -1,5 +1,6 @@
 function deepCopy(source, result) {
     if (!result) result = {};
+    if (typeof source != 'object'|| source instanceof Array || source === null) return result;
     for(var key in source) {
         if (!source.hasOwnProperty(key)) continue;
         if (source[key] !== null && typeof source[key] == 'object') {
